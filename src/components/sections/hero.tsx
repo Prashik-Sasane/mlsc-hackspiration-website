842881631e529c18b585a699774900d45ce8bfbf
Prashik-Sasane <prashiksasane85@gmail.com>
2026-02-03 15:20:58 +0530

    hello

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -65,7 +65,7 @@ export function Hero() {
     "
               >
                 <StaggeredText
-                  text="A two-phase hybrid hackathon, with an online round on 14th February followed by an offline finale on 15th February."
+                  text="A thrilling one-day hybrid hackathon merging innovation with game-show excitement"
                   delay={0.01}
                   staggerDelay={0.01}
                   className="text-md md:text-lg font-mono justify-center"
