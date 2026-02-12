ee3679e2a39553ac3f9351735b0b29555e9d4397
Prashik-Sasane <prashiksasane85@gmail.com>
2026-02-12 20:28:16 +0530

    feat: add description in hero section

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -65,8 +65,7 @@ export function Hero() {
     "
               >
                 <StaggeredText
-
-                  text="A hybrid hackathon featuring an online round on 14th February and an offline finale on 15th February"
+                  text="A thrilling one-day hybrid hackathon merging innovation with game-show excitement"
                   delay={0.01}
                   staggerDelay={0.01}
                   className="text-md md:text-lg font-mono justify-center"
